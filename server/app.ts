fd940bad015dc2e5df751a61039f2dc741162193
Adam-J <jasnypelnyfull@gmail.com>
2024-09-24 22:08:57 +0200

    successful unknown routes testing

--- server/app.ts
+++ server/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { NextFunction, Request, Response } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
@@ -16,6 +17,12 @@ app.use(cors({ origin: process.env.ORIGIN }));
 // testing api
 app.get("/test", (req, res) => {
   res.send("API is Working!");
+});
+
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+  const err = new Error(`Route ${req.originalUrl} not Found!`);
+  res.status(400);
+  next(err);
 });
 
 export default app;
