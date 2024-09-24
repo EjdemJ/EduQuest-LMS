9be24f2a809c1156277764f71c2f7307bb1dba14
Adam-J <jasnypelnyfull@gmail.com>
2024-09-24 18:12:55 +0200

    fixed cors, checked backend to front end connection

--- server/app.ts
+++ server/app.ts
@@ -1,5 +1,39 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
 export default app;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/users", (req, res) => {
+  const users = [
+    {
+      name: "user 1",
+      id: 1,
+    },
+    {
+      name: "user 2",
+      id: 2,
+    },
+    {
+      name: "user 3",
+      id: 3,
+    },
+    {
+      name: "user 4",
+      id: 4,
+    },
+  ];
+  res.json(users);
+});
+
+app.get("/greetings", (req, res) => {
+  const greeting = "Hello World!";
+  res.json(greeting);
+});
